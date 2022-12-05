32866b673960fe79d2611ed62ba59f1cd1d21896
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-12-05 10:55:02 -0500

    Fixed the issue regarding Site Administration + Added rest_framework to installed apps + Added products to installed apps

--- products/models.py
+++ products/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    class Meta:
-        app_label = 'Products'
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
