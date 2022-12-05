98c11970df02614a36370cc2604d9863e86d2f8d
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-12-04 21:26:49 -0500

    Fixed problem with Runtime Error + I added an app_label and created a Meta class within my Products Model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    class Meta:
+        app_label = 'Product'
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
